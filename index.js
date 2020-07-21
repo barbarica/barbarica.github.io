33bc6a17a0e9cba622b0fb06aa8f074e6aaa06a6
Asapanna Rakesh <rakeshark22@gmail.com>
2020-07-21 13:04:41 +0530

    added

---
+++ index.js
@@ -0,0 +1,9 @@
+const container = document.getElementById('container')
+
+if ( ( window.innerWidth > 800 ) && ( window.innerHeight > 600)) {
+    // desktop
+    console.log('desk')
+    container.style.width = "65%"
+} else {
+    // mob
+}
