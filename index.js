e7c23ead5ae3ac1ad81f7cfbc4f7142d4a575c1a
Asapanna Rakesh <rakeshark22@gmail.com>
2020-07-22 00:27:20 +0530

    plates added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const body          = document.getElementById('body')
 if ( ( window.innerWidth > 800 ) && ( window.innerHeight > 600)) {
     // desktop
     console.log('desk')
-    container.style.width = "65%"
+    container.style.width = "60%"
 } else {
     // mob
     body.style.fontSize = "1.4em"
