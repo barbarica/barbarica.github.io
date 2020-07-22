3f3e809457ada4ce6666c780ef73d86f0b971daa
Asapanna Rakesh <rakeshark22@gmail.com>
2020-07-22 09:32:16 +0530

    materialize override + fab

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ if ( ( window.innerWidth > 800 ) && ( window.innerHeight > 600)) {
     // mob
     body.style.fontSize = "1.4em"
 }
+
+// fab
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {direction: 'left'});
+    //instance.open();
+    //instance.close();
+});
