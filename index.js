1f0a587aca621aeb473c5b0dca39f64a66d531bc
Asapanna Rakesh <rakeshark22@gmail.com>
2020-07-22 17:16:43 +0530

    added fbas, jquery todo: material phone layout

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const body          = document.getElementById('body')
 if ( ( window.innerWidth > 800 ) && ( window.innerHeight > 600)) {
     // desktop
     console.log('desk')
-    container.style.width = "60%"
+    container.style.maxWidth = "60vw"
 } else {
     // mob
     body.style.fontSize = "1.4em"
@@ -16,4 +16,43 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.FloatingActionButton.init(elems, {direction: 'left'});
     //instance.open();
     //instance.close();
+});
+
+
+$(document).ready(function(){
+    $('.floatingButton').on('click',
+        function(e){
+            e.preventDefault();
+            $(this).toggleClass('open');
+            if($(this).children('.fa').hasClass('fa-plus'))
+            {
+                $(this).children('.fa').removeClass('fa-plus');
+                $(this).children('.fa').addClass('fa-close');
+            } 
+            else if ($(this).children('.fa').hasClass('fa-close')) 
+            {
+                $(this).children('.fa').removeClass('fa-close');
+                $(this).children('.fa').addClass('fa-plus');
+            }
+            $('.floatingMenu').stop().slideToggle();
+        }
+    );
+    $(this).on('click', function(e) {
+        var container = $(".floatingButton");
+
+        // if the target of the click isn't the container nor a descendant of the container
+        if (!container.is(e.target) && $('.floatingButtonWrap').has(e.target).length === 0) 
+        {
+            if(container.hasClass('open'))
+            {
+                container.removeClass('open');
+            }
+            if (container.children('.fa').hasClass('fa-close')) 
+            {
+                container.children('.fa').removeClass('fa-close');
+                container.children('.fa').addClass('fa-plus');
+            }
+            $('.floatingMenu').hide();
+        }
+    });
 });
