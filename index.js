75a489f6c17228a0aeb4ff1a7435666beb9b0430
Asapanna Rakesh <rakeshark22@gmail.com>
2020-07-23 13:55:54 +0530

    added

--- index.js
+++ index.js
@@ -3,36 +3,10 @@ const body          = document.getElementById('body')
 var fc              = document.getElementsByClassName('fc')
 var listFab         = document.getElementById('list-fab')
 var circleFab       = document.getElementById('circle-fab')
+var sbInput         = document.getElementById('sb-input')
+var sb              = document.getElementById('sb')
 
-if ( ( window.innerWidth > 800 ) && ( window.innerHeight > 600)) {
-    // desktop
-    console.log('desk')
-    container.style.maxWidth = "60vw"
-} else {
-    // mob
-    body.style.fontSize = "1.4em"
-    document.body.style.backgroundImage = "url('img_tree.png')";
-
-    // float a-button
-    listFab.style.right = '2%'
-    circleFab.style.right = '20%'
-
-    // flip bars
-    for(let i=0; i<fc.length; i++){
-        fc[i].style.display = "none"
-    }
-}
-
-// fab
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {direction: 'left'});
-    //instance.open();
-    //instance.close();
-    
-});
-
-
+// config fab list
 $(document).ready(function(){
     $('.floatingButton').on('click',
         function(e){
@@ -70,3 +44,125 @@ $(document).ready(function(){
         }
     });
 });
+// fab list end
+
+// fab
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {direction: 'left'});
+    //instance.open();
+    //instance.close();
+    
+});
+
+
+// Desktop  / Mob
+if ( ( window.innerWidth > 800 ) && ( window.innerHeight > 600)) {
+    // desktop
+    console.log('desk')
+    container.style.maxWidth = "60vw"
+
+    // search bar transition
+    sb.style.webkitTransition = "width 0.4s ease-in-out"
+    sb.style.transition = "width 0.4s ease-in-out"
+
+} else {
+    // mob
+    body.style.fontSize = "1.4em"
+    document.body.style.backgroundImage = "url('img_tree.png')";
+    container.style.marginTop = "5em"
+
+    // float a-button
+    listFab.style.right = '2%'
+    circleFab.style.right = '20%'
+
+    // flip bars
+    for(let i=0; i<fc.length; i++){
+        fc[i].style.display = "none"
+    }
+
+    // search bar input
+    sb.style.width = "90vw";
+    sb.style.top = "2em"
+    /*sbInput.style.borderBottom = "1px solid black"*/ 
+}
+
+
+
+
+// sb input dynamics start
+function onSearchClick(){
+    sbInput.innerHTML = "&#xF002; &nbsp;"
+    cursorManager.setEndOfContenteditable(sbInput);
+}
+// sb input dynamics end
+
+
+
+/// helper
+//Namespace management idea from http://enterprisejquery.com/2010/10/how-good-c-habits-can-encourage-bad-javascript-habits-part-1/
+(function( cursorManager ) {
+
+    //From: http://www.w3.org/TR/html-markup/syntax.html#syntax-elements
+    var voidNodeTags = ['AREA', 'BASE', 'BR', 'COL', 'EMBED', 'HR', 'IMG', 'INPUT', 'KEYGEN', 'LINK', 'MENUITEM', 'META', 'PARAM', 'SOURCE', 'TRACK', 'WBR', 'BASEFONT', 'BGSOUND', 'FRAME', 'ISINDEX'];
+
+    //From: https://stackoverflow.com/questions/237104/array-containsobj-in-javascript
+    Array.prototype.contains = function(obj) {
+        var i = this.length;
+        while (i--) {
+            if (this[i] === obj) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Basic idea from: https://stackoverflow.com/questions/19790442/test-if-an-element-can-contain-text
+    function canContainText(node) {
+        if(node.nodeType == 1) { //is an element node
+            return !voidNodeTags.contains(node.nodeName);
+        } else { //is not an element node
+            return false;
+        }
+    };
+
+    function getLastChildElement(el){
+        var lc = el.lastChild;
+        while(lc && lc.nodeType != 1) {
+            if(lc.previousSibling)
+                lc = lc.previousSibling;
+            else
+                break;
+        }
+        return lc;
+    }
+
+    //Based on Nico Burns's answer
+    cursorManager.setEndOfContenteditable = function(contentEditableElement)
+    {
+
+        while(getLastChildElement(contentEditableElement) &&
+              canContainText(getLastChildElement(contentEditableElement))) {
+            contentEditableElement = getLastChildElement(contentEditableElement);
+        }
+
+        var range,selection;
+        if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+        {    
+            range = document.createRange();//Create a range (a range is a like the selection but invisible)
+            range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
+            range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+            selection = window.getSelection();//get the selection object (allows you to change selection)
+            selection.removeAllRanges();//remove any selections already made
+            selection.addRange(range);//make the range you have just created the visible selection
+        }
+        else if(document.selection)//IE 8 and lower
+        { 
+            range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+            range.moveToElementText(contentEditableElement);//Select the entire contents of the element with the range
+            range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+            range.select();//Select the range (make it the visible selection
+        }
+    }
+
+}( window.cursorManager = window.cursorManager || {}));
