248411e0fb798ed9432229e4afd8cd16bc902582
Asapanna Rakesh <rakeshark22@gmail.com>
2020-08-09 06:33:16 +0530

    todo: nav toggles dependant to scroll

--- parason/modules/js/custom.js
+++ parason/modules/js/custom.js
@@ -14,3 +14,18 @@ if ( ( window.innerWidth > 800 ) && ( window.innerHeight > 600)) {
 navToggle.addEventListener('click', ()=>{
     navContainer.style.height = ""
 })
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;  
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.display = "flex";
+    
+  } else {
+    document.getElementById("navbar").style.display = "none";
+    //console.log(navToggle.classList.contains('collapsed'))
+  }
+  
+ prevScrollpos = currentScrollPos;
+}
