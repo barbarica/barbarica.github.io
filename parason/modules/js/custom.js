e5371aa0eb651e4697d4b4d0a20aea00271fa441
Asapanna Rakesh <rakeshark22@gmail.com>
2020-08-10 16:17:12 +0530

    nav bar fixed

--- parason/modules/js/custom.js
+++ parason/modules/js/custom.js
@@ -6,15 +6,26 @@ const navDropdown    = document.getElementById('navbarSupportedContent')
 
 if ( ( window.innerWidth > 800 ) && ( window.innerHeight > 600)) {
     // desktop
-    navContainer.style.height = "4vh"
+    navContainer.style.height = "3vh"
 } else {
     // mob
     navContainer.style.height = "7vh"
     navToggle.style.margin = "0"
 }
 
-navToggle.addEventListener('click', ()=>{
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+var toggleClick = false
+navToggle.addEventListener('click', async ()=>{
+  if (!toggleClick){
     navContainer.style.height = ""
+  } else {
+    // mob (only)
+    await sleep(300);
+    navContainer.style.height = "7vh"
+  }
+  toggleClick = !toggleClick
 })
 
 
@@ -22,11 +33,14 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;  
   if (prevScrollpos > currentScrollPos) {
+    // scroll up
     mainMenu.style.top = "-72px"; // unhide
   } else {
-    mainMenu.style.top = "-145px"; // hide
-    navDropdown.classList.remove("show")
-    navToggle.ariaExpanded = "false"
+    // scroll down
+    if (!navDropdown.classList.contains("show")){
+      // hide only when nav options not open
+      mainMenu.style.top = "-145px"; 
+    }
   }
   
  prevScrollpos = currentScrollPos;
